8d1f5df2029d040936ba999b33d64483d5a10d17
PritamDutta <webdevpritam7498@gmail.com>
2023-07-27 13:16:38 +0600

    deleted bookings data from ui success

--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -1,21 +1,76 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
+import BookingsData from "./BookingsData";
+import Swal from "sweetalert2";
 
 const Bookings = () => {
   const { user } = useContext(AuthContext);
-  const[bookings, setBookings] = useState([]);
+  const [bookings, setBookings] = useState([]);
   console.log(bookings);
 
   const url = `http://localhost:5000/bookings?email=${user?.email}`;
 
   useEffect(() => {
     fetch(url)
-    .then(res=> res.json())
-    .then(data=>setBookings(data))
+      .then((res) => res.json())
+      .then((data) => setBookings(data));
   }, []);
+
+  const handleDelete = (id) => {
+    // console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/bookings/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            // console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "booking service has been deleted.", "success");
+              const remaining = bookings.filter(booking => booking._id !== id)
+              setBookings(remaining)
+            }
+          });
+        
+      }
+    });
+  };
   return (
     <div>
       <h3>here you can see booking table! {bookings.length}</h3>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr className="bg-slate-400 text-black font-bold">
+              <th></th>
+              <th>Service & date</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Price</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+                bookings.map(booking => <BookingsData key={booking._id} booking={booking} handleDelete={handleDelete} ></BookingsData>)
+            }
+            
+            
+           
+          </tbody>
+          
+        </table>
+      </div>
     </div>
   );
 };
---
+++ src/Pages/Bookings/BookingsData.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const BookingsData = ({ booking, handleDelete }) => {
+  const { customerName, img, price, date, email, service, service_id, _id } =
+    booking;
+
+
+  return (
+    <tr>
+      <th>
+        <button
+          onClick={() => handleDelete(_id)}
+          className="btn btn-sm btn-circle btn-outline "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </th>
+      <td>
+        <div className="flex items-center space-x-3">
+          <div className="avatar">
+            <div className="rounded-md w-28 ">
+              <img src={img} alt="Service img" />
+            </div>
+          </div>
+          <div>
+            <h2 className="font-bold">{service}</h2>
+            <div className="text-sm opacity-50">{date}</div>
+          </div>
+        </div>
+      </td>
+      <td>{customerName}</td>
+      <td>{email}</td>
+      <td>{price}</td>
+      <th>
+        <button className="btn btn-error btn-sm">Pending</button>
+      </th>
+    </tr>
+  );
+};
+
+export default BookingsData;
